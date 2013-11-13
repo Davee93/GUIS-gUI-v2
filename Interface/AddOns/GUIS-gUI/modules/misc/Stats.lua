027830ffd2b06afd2f9ce8aba89ef075adc8a265
Kkthnx <JRussell20@gmail.com>
2013-11-13 06:36:43 -0500

    Forgot this is not retail :)

--- Interface/AddOns/GUIS-gUI/modules/misc/Stats.lua
+++ Interface/AddOns/GUIS-gUI/modules/misc/Stats.lua
@@ -2,163 +2,171 @@
 --	Auto hide unnecessary stats from CharacterFrame(module from Inomena by p3lim)
 ----------------------------------------------------------------------------------------
 
+-- 4.3.4 -- Do not touch unless you know what you are doing.
+
 PAPERDOLL_STATCATEGORIES = {
 	GENERAL = {
 		id = 1,
 		stats = {
-			"ITEMLEVEL",
-			"MOVESPEED",
+			'ITEMLEVEL',
+			'MOVESPEED',
 		},
 	},
 	MELEE = {
 		id = 2,
 		stats = {
-			"STRENGTH",
-			"AGILITY",
-			"MELEE_AP",
-			"ENERGY_REGEN",
-			"RUNE_REGEN",
-			"HASTE",
-			"CRITCHANCE",
-			"HITCHANCE",
-			"EXPERTISE",
-			"MASTERY",
+			'STRENGTH',
+			'AGILITY',
+			'MELEE_AP',
+			'ENERGY_REGEN',
+			'RUNE_REGEN',
+			'HASTE',
+			'CRITCHANCE',
+			'HITCHANCE',
+			'EXPERTISE',
+			'MASTERY',
 		},
 	},
 	RANGED = {
 		id = 2,
 		stats = {
-			"AGILITY",
-			"RANGED_AP",
-			"RANGED_HASTE",
-			"FOCUS_REGEN",
-			"CRITCHANCE",
-			"RANGED_HITCHANCE",
-			"EXPERTISE",
-			"MASTERY",
+			'AGILITY',
+			'RANGED_AP',
+			'RANGED_HASTE',
+			'FOCUS_REGEN',
+			'RANGED_CRITCHANCE',
+			'RANGED_HITCHANCE',
+			'MASTERY',
 		},
 	},
 	SPELL = {
 		id = 2,
 		stats = {
-			"SPIRIT",
-			"INTELLECT",
-			"SPELLDAMAGE",
-			"SPELLHEALING",
-			"SPELL_HASTE",
-			"MANAREGEN",
-			"SPELLCRIT",
-			"SPELL_HITCHANCE",
-			"MASTERY",
+			'SPIRIT',
+			'INTELLECT',
+			'SPELLDAMAGE',
+			'SPELLHEALING',
+			'SPELL_HASTE',
+			'MANAREGEN',
+			'SPELL_CRITCHANCE',
+			'SPELL_HITCHANCE',
+			'MASTERY',
 		},
 	},
 	DEFENSE = {
 		id = 3,
 		stats = {
-			"STAMINA",
-			"ARMOR",
-			"DODGE",
-			"PARRY",
-			"BLOCK",
-			"RESILIENCE_REDUCTION",
-			"PVP_POWER",
+			'STAMINA',
+			'ARMOR',
+			'DODGE',
+			'PARRY',
+			'BLOCK',
+			'RESILIENCE_REDUCTION',
+			'ARCANE',
+			'FIRE',
+			'FROST',
+			'NATURE',
+			'SHADOW',
 		},
 	},
 }
 
 local orig = PaperDoll_InitStatCategories
-local class = select(3, UnitClass("player"))
+local class = select(2, UnitClass('player'))
 
 local sort = {
 	{
-		"GENERAL",
-		"MELEE",
-		"DEFENSE",
+		'GENERAL',
+		'MELEE',
+		'DEFENSE',
 	},
 	{
-		"GENERAL",
-		"RANGED",
-		"DEFENSE",
+		'GENERAL',
+		'RANGED',
+		'DEFENSE',
 	},
 	{
-		"GENERAL",
-		"SPELL",
-		"DEFENSE",
+		'GENERAL',
+		'SPELL',
+		'DEFENSE',
 	},
 }
 
 local spec
-local specs = {
-	{1, 1, 1},
-	{3, 1, 1},
-	{2, 2, 2},
-	{1, 1, 1},
-	{3, 3, 3},
-	{1, 1, 1},
-	{3, 1, 3},
-	{3, 3, 3},
-	{3, 3, 3},
-	{1, 3, 1},
-	{3, 1, 1, 3},
+local classes = {
+	DEATHKNIGHT = {1, 1, 1},
+	DRUID = {3, 1, 3},
+	HUNTER = {2, 2, 2},
+	MAGE = {3, 3, 3},
+	PALADIN = {3, 1, 1},
+	PRIEST = {3, 3, 3},
+	ROGUE = {1, 1, 1},
+	SHAMAN = {3, 1, 3},
+	WARLOCK = {3, 3, 3},
+	WARRIOR = {1, 1, 1},
 }
 
-local handler = CreateFrame("Frame")
-handler:RegisterEvent("PLAYER_TALENT_UPDATE")
-handler:RegisterEvent("ACTIVE_TALENT_GROUP_CHANGED")
-handler:SetScript("OnEvent", function()
-	spec = GetSpecialization()
+local handler = CreateFrame('Frame')
+handler:RegisterEvent('PLAYER_TALENT_UPDATE')
+handler:RegisterEvent('ACTIVE_TALENT_GROUP_CHANGED')
+handler:SetScript('OnEvent', function()
+	local tabs = GetNumTalentTabs()
+	if(tabs == 0) then return end
 
-	if spec then
-		PaperDoll_InitStatCategories = function()
-			orig(sort[specs[class][spec]], nil, nil, "player")
-			PaperDollFrame_CollapseStatCategory(CharacterStatsPaneCategory4)
+	local mostPoints = -1
+	for index = 1, tabs do
+		local _, _, _, _, points = GetTalentTabInfo(index)
+		if(points > mostPoints) then
+			mostPoints = points
+			spec = index
 		end
+	end
+
+	PaperDoll_InitStatCategories = function()
+		orig(sort[classes[class][spec]], nil, nil, 'player')
+		PaperDollFrame_CollapseStatCategory(CharacterStatsPaneCategory4)
 	end
 end)
 
 for index = 1, 3 do
-	local toolbar = _G["CharacterStatsPaneCategory"..index.."Toolbar"]
-	toolbar:SetScript("OnEnter", nil)
-	toolbar:SetScript("OnClick", nil)
+	local toolbar = _G['CharacterStatsPaneCategory' .. index .. 'Toolbar']
+	toolbar:SetScript('OnEnter', nil)
+	toolbar:SetScript('OnClick', nil)
 	toolbar:RegisterForDrag()
 end
 
 do
 	local setStat = PaperDollFrame_SetStat
-	function PaperDollFrame_SetStat(self, ...)
-		if index == 1 and class ~= 6 and class ~= 2 and class ~= 1 then
+	function PaperDollFrame_SetStat(self, unit, index)
+		if(index == 1 and class ~= 'DEATHKNIGHT' and class ~= 'PALADIN' and class ~= 'WARRIOR') then
 			return self:Hide()
 		end
 
-		setStat(self, ...)
+		setStat(self, unit, index)
 	end
 
 	local setSpellHit = PaperDollFrame_SetSpellHitChance
-	function PaperDollFrame_SetSpellHitChance(self, ...)
-		if class == 5 and spec ~= 3 then
+	function PaperDollFrame_SetSpellHitChance(self, unit)
+		if(class == 'PRIEST' and spec ~= 3) then
 			return self:Hide()
-		elseif (class == 11 or class == 7) and spec == 3 then
+		elseif((class == 'DRUID' or class == 'SHAMAN') and spec == 3) then
 			return self:Hide()
 		end
 
-		setSpellHit(self, ...)
+		setSpellHit(self, unit)
 	end
 
 	local setParry = PaperDollFrame_SetParry
-	function PaperDollFrame_SetParry(self, ...)
-		if class ~= 2 and class ~= 1 and class ~= 6 and not (class == 10 and spec == 2) then
+	function PaperDollFrame_SetParry(self, unit)
+		if(class ~= 'PALADIN' and class ~= 'WARRIOR' and class ~= 'DEATHKNIGHT') then
 			return self:Hide()
 		end
-
-		setParry(self, ...)
 	end
 
 	local setBlock = PaperDollFrame_SetBlock
-	function PaperDollFrame_SetBlock(self, ...)
-		if class ~= 2 and class ~= 1 then
+	function PaperDollFrame_SetBlock(self, unit)
+		if(class ~= 'PALADIN' and class ~= 'WARRIOR') then
 			return self:Hide()
 		end
-
-		setBlock(self, ...)
 	end
 end
